c3e2835fcfb992e795ea355aa2534526385a0a68
Rayann <rayann.karon@iut-rodez.fr>
2023-04-14 09:03:55 +0200

    feat: Ajout de la recyclerView

--- app/src/main/java/com/example/partagedefrais/MainActivity.java
+++ app/src/main/java/com/example/partagedefrais/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.partagedefrais;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.partagedefrais.adapter.DepenseAdapter;
 import com.example.partagedefrais.dao.DepenseDao;
 import com.example.partagedefrais.dao.UtilisateurDao;
 import com.example.partagedefrais.model.Depense;
 import com.example.partagedefrais.model.Utilisateur;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,23 +22,52 @@ public class MainActivity extends AppCompatActivity {
 
     /** Objet destiné à faciliter l'accès à la table des dépenses */
     private DepenseDao accesDepense;
+    /**
+     * Element permettant d'afficher la liste des photos
+     */
+    private RecyclerView depenseRecyclerView;
+    private ArrayList<Depense> listeDepense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        depenseRecyclerView = findViewById(R.id.recycler_depense);
+
+        initialiseDatas();
+
+        /*
+         * On crée un gestionnaire de layout linéaire, et on l'associe à la
+         * liste de type RecyclerView
+         */
+        LinearLayoutManager gestionnaireLineaire = new LinearLayoutManager(this);
+        depenseRecyclerView.setLayoutManager(gestionnaireLineaire);
+
+        /*
+         * On crée un adaptateur personnalisé et permettant de gérer spécifiquement
+         * l'affichage des instances de type PhotoParis en tant que item de la liste.
+         * Cet adapatateur est associé au RecyclerView
+         */
+        DepenseAdapter adaptateur = new DepenseAdapter(listeDepense);
+        depenseRecyclerView.setAdapter(adaptateur);
+
+
+    }
+
+    /**
+     * Méthode pour initialiser la liste des photos et des textes
+     */
+    private void initialiseDatas() {
         accesUtilisateur = UtilisateurDao.getInstance(this);
         accesUtilisateur.open();
-
-        accesUtilisateur.insert(new Utilisateur(0,"jonny"));
-        accesUtilisateur.insert(new Utilisateur(0,"albert"));
 
         accesDepense = DepenseDao.getInstance(this);
         accesDepense.open();
 
-        Utilisateur jonny = accesUtilisateur.get("jonny");
-        accesDepense.insert(new Depense(0, "bar", 10.0),jonny.getId());
+        ArrayList<Utilisateur> users = accesUtilisateur.getAll();
+
+        listeDepense = accesDepense.getAll();
     }
 
     @Override
---
+++ app/src/main/java/com/example/partagedefrais/holder/DepenseViewHolder.java
@@ -0,0 +1,49 @@
+/* DepenseViewHolder.java                                         12 avr. 2023
+ * IUT Rodez, no copyright
+ */
+package com.example.partagedefrais.holder;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.partagedefrais.R;
+import com.example.partagedefrais.model.Depense;
+
+/**
+ * Description du contenant des items de la liste de type RecyclerView
+ * permettant d'afficher les dépenses.
+ * @author rayann.karon
+ */
+public class DepenseViewHolder  extends RecyclerView.ViewHolder{
+
+    private final TextView utilisateur;
+    private final TextView nomDepense;
+    private final TextView montantDepense;
+
+    /**
+     * Constructeur avec en argument une vue correspondant
+     * à un item de la liste
+     * Le constructeur permet d'initialiser les identifiants des
+     * widgets déclarés en tant qu'attributs
+     * @param itemView vue décrivant l'affichage d'un item de la liste
+     */
+    public DepenseViewHolder(View itemView) {
+        super(itemView);
+        utilisateur = itemView.findViewById(R.id.utilisateur);
+        nomDepense = itemView.findViewById(R.id.nom_depense);
+        montantDepense = itemView.findViewById(R.id.montant_depense);
+    }
+
+    /**
+     * Permet de placer les informations contenues dans l'argument
+     * dans les widgets d'un item de la liste
+     * @param depense l'instance qui doit être affichée
+     */
+    public void bind(Depense depense){
+        utilisateur.setText("");
+        nomDepense.setText(depense.getNom());
+        montantDepense.setText(depense.getMontant() + "€");
+    }
+}
